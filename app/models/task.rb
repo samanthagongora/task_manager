88bff4830ca64a6e9302e3d807ebf4a196769eee
Samantha Gongora <samantha.gongora@gmail.com>
2017-06-22 16:26:56 -0600

    adds links to index.erd and adds ID to task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,12 +1,13 @@
 require 'sqlite3'
 
 class Task
-  attr_reader :title, :description
+  attr_reader :title, :description, :id
   def initialize(task_params)
     @description = task_params["description"]
     @title       = task_params["title"]
     @database = SQLite3::Database.new('db/task_manager_development.db')
     @database.results_as_hash = true
+    @id = task_params["id"] if task_params["id"]
   end
 
   def save
@@ -21,7 +22,7 @@ class Task
       Task.new(task)
     end
   end
- 
+
   def self.find(id)
     database = SQLite3::Database.new('db/task_manager_development.db')
     database.results_as_hash = true
