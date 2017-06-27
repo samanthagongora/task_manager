bf743c5d923d0453e80855491c1532ec0e4f4744
Samantha Gongora <samantha.gongora@gmail.com>
2017-06-26 21:21:13 -0600

    adds update feature

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -2,6 +2,7 @@ require_relative '../models/task.rb'
 
 class TaskManagerApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
+   set :method_override, true
 
   get '/' do
     erb :dashboard
@@ -16,11 +17,20 @@ end
     erb :new
   end
 
-  get '/tasks/:id' do
-    @task = Task.find(params[:id])
-    erb :show
-  end
+  # get '/tasks/:id' do
+  #   @task = Task.find(params[:id])
+  #   erb :show
+  # end
 
+  get '/tasks/:id/edit' do
+   @task = Task.find(params[:id])
+   erb :edit
+ end
+
+  put '/tasks/:id' do |id|
+    Task.update(id.to_i, params[:task])
+    redirect "/tasks/#{id}"
+  end
 
   post '/tasks' do
     task = Task.new(params[:task])
